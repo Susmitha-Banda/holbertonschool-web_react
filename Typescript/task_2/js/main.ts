3cdcfd3ff4caeaf9645dffd4516bad6410d23062
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-09-24 14:31:58 +1000

    Task 7- String literal types

--- Typescript/task_2/js/main.ts
+++ Typescript/task_2/js/main.ts
@@ -75,3 +75,13 @@ function executeWork(employee: Director | Teacher): void {
 
 executeWork(createEmployee(200));
 executeWork(createEmployee(1000));
+
+
+// Task 7
+type Subjects = "Math" | "History"
+
+function teachClass(todayClass: Subjects): string {
+  return (todayClass == "Math") ? "Teaching Math" : "Teaching History"
+}
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
